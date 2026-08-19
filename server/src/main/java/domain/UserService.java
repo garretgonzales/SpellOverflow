a65a6a9f2ea12c147be60043df60d66a1bcf145c
garretgonzales <garret915@gmail.com>
2026-08-19 16:57:49 -0500

    Began working on user auth feature, added user repo and service wiring

---
+++ server/src/main/java/domain/UserService.java
@@ -0,0 +1,14 @@
+package domain;
+
+
+import data.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
