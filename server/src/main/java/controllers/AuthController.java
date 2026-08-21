581e2a93795768ee4c046c5db1df996bdc9adc39
garretgonzales <garret915@gmail.com>
2026-08-21 09:42:34 -0500

    public registration endpoint established, all tests passing

---
+++ server/src/main/java/controllers/AuthController.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import domain.UserService;
+import dto.RegisterUserRequest;
+import dto.RegisterUserResponse;
+import models.User;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterUserResponse> register(
+            @Valid @RequestBody RegisterUserRequest request
+    ) {
+        User user = userService.register(request);
+
+        RegisterUserResponse response = new RegisterUserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail()
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
